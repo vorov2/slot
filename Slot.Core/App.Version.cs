ffb0f4d800aab400f8b368aaf733e967e9de708b
Vasily Voronkov <basil@voronkov.name>
2016-12-28 17:17:57 +0300

    -

--- Slot.Core/App.Version.cs
+++ Slot.Core/App.Version.cs
@@ -4,8 +4,8 @@ namespace Slot.Core
 {
     public static partial class App
     {
-        public const string Version = "0.1.0.515";
+        public const string Version = "0.1.0.516";
         public const string BuildDate = "2016-12-28";
-        public const string Commit = "b59ef29e20cb42fcfc858110f5108145a6a115e4";
+        public const string Commit = "96b44680f5d3942744e88a3fcaa97699d64c237c";
     }
 }
